5cb9c5d82f5abd73460289c24eb28431bfbf2ef1
Lucas Latsch <bello-lucas@hotmail.com>
2025-04-03 20:02:51 -0300

    Add environment configuration for API URLs

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,8 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+// Define a URL da API com base na variável de ambiente
+export const api = `${process.env.REACT_APP_API_URL}/api`;
+export const uploads = `${process.env.REACT_APP_API_URL}/uploads`;
 
+// Configuração para requisições HTTP
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
 
